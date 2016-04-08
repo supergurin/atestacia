444bcef223fe21642a730c2aa71f3610b3f4cd45
supergurin <illya_gurin1998@ukr.net>
2016-04-08 08:46:54 +0300

    Create 1lab.c

---
+++ 1lab.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+#include<math.h>
+
+int main()
+{
+
+	int x,y,d;
+	
+	printf ("Vvedite x");
+	scanf("%d",&x);
+	
+	printf("Vvedite y:");
+	scanf("%d",&y);
+	
+	d=sqrt(pow((x+35),2)+pow((y-40),2));
+	
+	
+	printf("Rastoyanie ravno%d"",Rastoyanie");
+	
+	return 0;
+}
